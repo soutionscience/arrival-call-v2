977f9ed7b4eed1d2c355d3109b4d8e22678742b4
soutionscience <marshnjagi@yahoo.com>
2020-04-16 14:12:57 +0300

    init arrival call project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./PAGES/welcome/welcome.module').then( m => m.WelcomePageModule)
   }
 ];
 @NgModule({
