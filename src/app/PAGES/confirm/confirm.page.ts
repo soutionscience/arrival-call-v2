f309b7353213f32eeee098435ed594120e4f842a
soutionscience <marshnjagi@yahoo.com>
2020-05-21 14:20:41 +0300

    added alert

--- src/app/PAGES/confirm/confirm.page.ts
+++ src/app/PAGES/confirm/confirm.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PickerController } from '@ionic/angular';
+import { PickerController, AlertController } from '@ionic/angular';
 import { PickerOptions } from "@ionic/core";
 
 @Component({
@@ -7,6 +7,9 @@ import { PickerOptions } from "@ionic/core";
   templateUrl: './confirm.page.html',
   styleUrls: ['./confirm.page.scss'],
 })
+
+
+  // remember to check distance to determing geo boundary
 
 export class ConfirmPage implements OnInit {
   slideOpts = {
@@ -16,9 +19,10 @@ export class ConfirmPage implements OnInit {
    // centeredSlides: true,
     speed: 400
   };
-  animals: string[] = ["Tiger", "Lion", "Elephant", "Fox", "Wolf"];
-  plants: string[] = ['cocoa', 'banana', 'potato', 'coconut']
- constructor(private pickerController: PickerController) { }
+  animals: number[] = [0, 1, 2, 3,];
+  plants: number[] = [0, 5, 10, 15, 20, 25,30, 35, 40, 45,50, 55, 60];
+ constructor(private pickerController: PickerController, private alertCtrl: AlertController) { }
+ selectedTime: any ={};
 
   ngOnInit() {
   }
@@ -33,18 +37,30 @@ export class ConfirmPage implements OnInit {
         {
           text:'Ok',
           handler:(value:any) => {
-            console.log(value);
+            console.log('value',value);
           }
         }
       ],
       columns:[{
-        name:'Animals',
+        name:'Hour',
         options:this.getColumnOptions()
       },
+      {
+        name: 'Hr',
+        options: [{text: 'Hr', value: 'Hr'}]
+
+      },
     {
-      name: 'Plants',
+      name: 'Minutes',
       options: this.getSecondColumnOptions()
-    }]
+    },
+
+    {
+      name: 'Mins',
+      options: [{text: 'Mins', value: null}]
+
+    }
+  ]
     };
 
     let picker = await this.pickerController.create(options);
@@ -65,9 +81,46 @@ export class ConfirmPage implements OnInit {
     })
     return options;
   }
+  getHour(){
+    let options =[]
+    options.push({text: 'Hr', val: null})
+    return options
+  }
 
 
+ //selected Time
+ selectTime(time){
+   this.selectedTime = {hour: time.hour, min: time.min}
+   console.log(' selected ', this.selectedTime);
+   this.presentAlertConfirm()
+ }
 
-  // remember to check distance to determing geo boundary
+ ///alert confirm
+
+ async presentAlertConfirm(){
+   const alert = await this.alertCtrl.create(
+     {
+       header: 'Confirm',
+       message: 'Alert you 3 mins before you arrive at Sarit center, rouderbut, westlands, Nairobi?',
+       buttons:[{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass:'secondary',
+        handler:()=>{
+          console.log('cancelled')
+        }
+       },
+       {text: 'Okay',
+      handler:()=>{
+        console.log('accepted')
+      }}
+
+        
+       ]
+     }
+   )
+   alert.present()
+ }
+
 
 }
